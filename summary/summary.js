9443e11f5d706de6d1229370aa384fd5bbe10121
Eva Timmer <evti@adifo.com>
2025-09-23 10:20:10 +0200

    summary dump

---
+++ summary/summary.js
@@ -0,0 +1,9 @@
+// querySelector uses CSS selectors
+document.querySelector("class.class") // finds first occurrence in document
+document.querySelector("#id")
+document.querySelector("#id1, #id2")
+// getElementById uses element ID
+document.getElementById("id")
+
+
+documentItem.addEventListener('event', functionToExecute, options)
